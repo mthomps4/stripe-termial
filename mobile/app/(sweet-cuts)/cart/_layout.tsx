3345c6104f6e7c5a35c0d96d0e72b8caf84f9c66
Matt Thompson <matt@echobind.com>
2025-05-12 16:48:43 -0400

    feat: Stripe Terminal SDK Provider wrapping cart

--- mobile/app/(sweet-cuts)/cart/_layout.tsx
+++ mobile/app/(sweet-cuts)/cart/_layout.tsx
@@ -1,6 +1,49 @@
+import { useGetConnectionToken } from "@/hooks/terminal/useGetConnectionToken";
+import {
+  StripeTerminalProvider,
+  useStripeTerminal,
+} from "@stripe/stripe-terminal-react-native";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { Text, View } from "react-native";
 
-export default function SweetCutsCartProvider() {
+export default function StripeTerminalWrapper() {
+  const { data: connectionToken, isLoading, error } = useGetConnectionToken();
+
+  if (isLoading) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  const secret = connectionToken?.secret;
+
+  if (!secret || error) {
+    return (
+      <View>
+        <Text>No connection token...</Text>
+      </View>
+    );
+  }
+
+  const tokenProvider = () => Promise.resolve(secret);
+
+  return (
+    <StripeTerminalProvider tokenProvider={tokenProvider}>
+      <CartStackLayout />
+    </StripeTerminalProvider>
+  );
+}
+
+const CartStackLayout = () => {
+  const { initialize } = useStripeTerminal();
+
+  useEffect(() => {
+    initialize();
+  }, [initialize]);
+
   return (
     <Stack initialRouteName="index" screenOptions={{ headerShown: false }}>
       <Stack.Screen
@@ -13,4 +56,4 @@ export default function SweetCutsCartProvider() {
       />
     </Stack>
   );
-}
+};
--- mobile/hooks/terminal/useGetConnectionToken.ts
+++ mobile/hooks/terminal/useGetConnectionToken.ts
@@ -1,15 +1,17 @@
 import { GET_CONNECTION_TOKEN_ROUTE } from "@/constants/apiRoutes";
 import { useCurrentUser } from "@/contexts/CurrentUserProvider";
+import { GetConnectionTokenResponse } from "@/types/terminal";
 import { useQuery } from "@tanstack/react-query";
 
 export function useGetConnectionToken() {
   const { token, user } = useCurrentUser();
 
-  return useQuery({
+  return useQuery<GetConnectionTokenResponse, Error>({
     queryKey: ["connection-token", user?.id],
     enabled: !!user?.id,
     queryFn: async () => {
       const response = await fetch(GET_CONNECTION_TOKEN_ROUTE, {
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
